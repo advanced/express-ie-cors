31c0134a5b7c0cdc8691745192abf89b241ce00b
advanced <rian@advanced.net.tw>
2013-01-15 14:12:01 +0800

    finished first draft

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @package     express-ie-cors
+ * @category    Express Middleware
+ * @version     0.9.1
+ * @copyright   Copyright (c) 2012-2013 - All rights reserved.
+ * @license     MIT License
+ * @author      Halid Rian <rian@advanced.net.tw>
+ *
+ */
+
+module.exports = require('./lib/express-ie-cors');
